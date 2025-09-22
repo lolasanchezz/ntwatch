594259f1d2b8a0d0988255dcb1818ba2bbc7fdf7
lolasanchezz <lolansanchez@icloud.com>
2025-09-22 14:24:24 -0400

    get pids function working

--- c_files/ex_ppi.c
+++ c_files/ex_ppi.c
@@ -3,49 +3,12 @@
 #include <unistd.h>
 #include <libproc.h>
 #include <sys/proc_info.h>
-/*
-int main(int argc, char *argv[]) {
-    /*
-    pid_t pid = getpid(); // Get current process ID
-    
-    // Step 1: Determine buffer size
-    int bufferSize = proc_pidinfo(pid, PROC_PIDLISTFDS, 0, NULL, 0);
-    if (bufferSize <= 0) {
-        perror("proc_pidinfo (size)");
-        return 1;
-    }
 
-    // Step 2: Allocate buffer and retrieve information
-    struct proc_fdinfo *fdInfo = (struct proc_fdinfo *)malloc(bufferSize);
-    if (fdInfo == NULL) {
-        perror("malloc");
-        return 1;
-    }
-
-    int bytesRead = proc_pidinfo(pid, PROC_PIDLISTFDS, 0, fdInfo, bufferSize);
-    if (bytesRead <= 0) {
-        perror("proc_pidinfo (data)");
-        free(fdInfo);
-        return 1;
-    }
-
-    int numberOfFDs = bytesRead / PROC_PIDLISTFD_SIZE;
-    printf("Open file descriptors for PID %d:\n", pid);
-    for (int i = 0; i < numberOfFDs; i++) {
-        printf("  FD: %d, Type: %d\n", fdInfo[i].proc_fd, fdInfo[i].proc_fdtype);
-    }
-
-    free(fdInfo);
-    return 0;
-    
-   getFDs(3010);
-}
-*/
 struct proc_fdinfo* getFDs(pid_t pid) {
-    /*
+    
     //get buffer size for file descriptors
     printf("%d", pid);
-    int bufferSize = proc_pidinfo(pid, PROC_PIDTASKALLINFO, 0, NULL, 0);
+    int bufferSize = proc_pidinfo(pid, PROC_PIDLISTFDS, 0, NULL, 0);
     if (bufferSize <= 0) {
         perror("proc_pidinfo (size)");
         exit(1);
@@ -66,27 +29,45 @@ struct proc_fdinfo* getFDs(pid_t pid) {
         free(fdInfo);
         return NULL;
     }
-*/
 
-    struct proc_fdinfo fds;
-
-    int bytes = proc_pidinfo(pid, PROC_PIDLISTFDS, 0, &fds, sizeof(fds));
-    if (bytes <= 0) {
-        perror("proc_pidinfo");
-    exit(1);
-    }
     
-    printf("%d,", fds);
+    for (int i = 0; i < (bytesRead / PROC_PIDLISTFD_SIZE); i++) {
+        if (fdInfo[i].proc_fdtype == 6) {
+            printf("%d \n", pid);
+        }
+    }
     //use proc_pidfdinfo once file descriptors are obtained
+    return NULL; 
+}
 
+
+pid_t* getPids() {
+
+    int pidCount = proc_listpids(PROC_ALL_PIDS, 0, NULL, 0);
+    unsigned long pidsBufSize = sizeof(pid_t) * (unsigned long)pidCount;
+    pid_t *pids = (pid_t *)malloc(pidsBufSize);
+
+    int bytesUsed = proc_listpids(PROC_ALL_PIDS, 0, pids, (int)pidsBufSize);
+    int numPids = bytesUsed / sizeof(pid_t);
+
+    for (int i = 0; i < numPids; i++) {
+        if (pids[i] != 0) {  // filter out "empty" slots, why does proc_listpids do this?
+            printf("PID: %d\n", pids[i]);
+        }
+    }
+
+    return pids;
 }
 
 
 
 
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) { 
+    pid_t myPid;
+    myPid = 94098;
+  
 
-    getFDs(1);
+    getPids();
 
 }
